177e8093b1f3d49fc4ddb2f0141941c20c4c217b
Janaina <jmtannus@gmail.com>
2024-02-20 23:51:38 -0300

    Implementando Letras grandes

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,16 @@ function App() {
           </div>
         </button>
       </header>
-      <div className="content-container"></div>
+      <div className="content-container">
+        <div className="heading-container">
+          <h2>Lets grow your</h2>
+          <h2>social presence.</h2>
+        </div>
+        <div className="bubble target">🎯</div>
+        <div className="bubble shop">🛍️</div>
+        <div className="bubble rocket">🚀</div>
+        <div className="bubble fire">🔥</div>
+      </div>
     </main>
   );
 }
